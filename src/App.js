522982ee2df25101e9124658ef3c04fbed429ebd
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-05-16 20:25:53 -0500

    routes component
    - added empty rockets component
    - added empty missions component
    - added empty profile component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import {
 } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/navbar/Navbar';
+import Missions from './Components/routes/Missions';
+import Rockets from './Components/routes/Rockets';
+import Profile from './Components/routes/Profile';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" />
-        <Route path="/categories" />
+        <Route path="/" Component={Rockets} />
+        <Route path="/missions" Component={Missions} />
+        <Route path="/profile" Component={Profile} />
       </Routes>
     </Router>
   );
