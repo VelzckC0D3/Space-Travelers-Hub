31acfd3c0a9df9b7966e2a0f2edbcdf0cd21eb80
Daniel Falcon <danfall963@gmail.com>
2023-05-17 15:34:57 -0400

    adding style with Rocket.css

--- src/Components/routes/Rockets.jsx
+++ src/Components/routes/Rockets.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import fetchRockets from '../redux/Rockets/FetchRockets';
+import '../../style/Rocket.css';
 
 const Rockets = () => {
   const dispatch = useDispatch();
@@ -12,17 +13,17 @@ const Rockets = () => {
 
   return (
     <div>
-      <ul>
+      <ul className="rockets-container">
         {rockets.map((rocket) => (
           <>
-            <li key={rocket.id}>
+            <li key={rocket.id} className="rocket-list">
               <div>
-                <img src={rocket.images[0]} alt="RocketImage" />
+                <img src={rocket.images[0]} alt="RocketImage" className="rocket-img" />
               </div>
-              <div>
-                <h2>{rocket.name}</h2>
-                <p>{rocket.description}</p>
-                <button type="button">Reserve Rocket</button>
+              <div className="rocket-inf">
+                <h2 className="rocket-name">{rocket.name}</h2>
+                <p className="rocket-des">{rocket.description}</p>
+                <button type="button" className="rocket-btn">Reserve Rocket</button>
               </div>
             </li>
           </>
