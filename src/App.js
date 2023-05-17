3e0230856a211c13c6f6baf56c6b4ce77e089673
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-05-16 19:56:53 -0500

    Header
    - implemented a new header with template navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/navbar/Navbar';
 
 function App() {
   return (
-    <div className="App" />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/categories" />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from 'react-router-dom';
+import '../../style/Navbar.css';
+import logo from '../../assets/Img/logo.png';
+
+const Navbar = () => (
+  <nav className="navCont">
+    <h1 className="navTitle">
+      <img src={logo} alt="Logo" className="navLogo" />
+      Space Travelers&apos; Hub
+    </h1>
+    <ul className="navUl">
+      {/* JUST TEMPLATE */}
+      <li className="navLi"><NavLink to="/" exact="true" activeclassname="active">Rockets</NavLink></li>
+      <li className="navLi"><NavLink to="/missions" activeclassname="active">Missions</NavLink></li>
+      <li className="navLi"><NavLink to="/profile" activeclassname="active">My Profile</NavLink></li>
+    </ul>
+  </nav>
+);
+export default Navbar;
