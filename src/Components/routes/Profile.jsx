fff5ec98ebeea9e45839c7d3474d46ef73a67945
Daniel Falcon <danfall963@gmail.com>
2023-05-19 05:01:06 -0400

    Fixing Linters Errors

--- src/Components/routes/Profile.jsx
+++ src/Components/routes/Profile.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import '../../style/Profile.css';
 
-const Profile = () => {
-  const joinedMissions = useSelector(
-    (state) => state.Missions.missions.filter((mission) => mission.reserved),
-  );
-
-  return (
-    <div>
-      <h1 className="missionsH1">Missions</h1>
-      <h2>Joined Missions:</h2>
-      <ul>
-        {joinedMissions.map((mission) => (
-          <li key={mission.id}>{mission.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+const Profile = () => (
+  <div>
+    <h1 className="profileH1">Profile</h1>
+  </div>
+);
 
 export default Profile;
